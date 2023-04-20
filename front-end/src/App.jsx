e396f958fb45f6c3a9f1291de23855b0412247dd
MaykeNeves <102259629+MaykeNeves@users.noreply.github.com>
2023-04-20 14:08:54 +0000

    (20/04) implementação do formulário de login

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,20 +2,25 @@ import './App.css'
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import HeaderBar from './components/ui/HeaderBar'
+import Box from '@mui/material/Box'
 
 function App() {
  
 
   return (
-    <div className="App">
+    
       
       <BrowserRouter>
+        <HeaderBar />
+        <Box sx={ {m:'25px auto'}}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
+        </Box>
       </BrowserRouter>
-    </div>
+    
   )
 }
 
